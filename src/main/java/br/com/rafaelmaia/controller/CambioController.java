fd6e1e698f85c7e9b8484b42bbfe6dc15c5a7ae2
Rafael Maia <106180433+rafaelmaiia@users.noreply.github.com>
2024-11-24 14:31:02 -0300

    removed mocks from controller and implemented currency conversion method

--- src/main/java/br/com/rafaelmaia/controller/CambioController.java
+++ src/main/java/br/com/rafaelmaia/controller/CambioController.java
@@ -1,6 +1,7 @@
 package br.com.rafaelmaia.controller;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.rafaelmaia.model.Cambio;
+import br.com.rafaelmaia.repository.CambioRepository;
 
 @RestController
 @RequestMapping("cambio-service")
@@ -18,6 +20,9 @@ public class CambioController {
 	@Autowired
 	private Environment environment;
 	
+	@Autowired
+	private CambioRepository repository;
+	
 	// http://localhost:8000/cambio-service/5/USD/BRL
 	@GetMapping(value = "/{amount}/{from}/{to}")
 	public Cambio getCambio(
@@ -25,9 +30,15 @@ public class CambioController {
 			@PathVariable("from") String from,
 			@PathVariable("to") String to) {
 		
-		var port = environment.getProperty("local.server.port");
+		var cambio = repository.findByFromAndTo(from, to);
+		if (cambio == null) throw new RuntimeException("Currency Unsupported");
 		
-		return new Cambio(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, port);
+		var port = environment.getProperty("local.server.port");
+		BigDecimal conversionFactor = cambio.getConversionFactor();
+		BigDecimal convertedValue = conversionFactor.multiply(amount);
+		cambio.setConvertedValue(convertedValue.setScale(2, RoundingMode.CEILING));
+		cambio.setEnvironment(port);
+		return cambio;
 	}
 
 }
