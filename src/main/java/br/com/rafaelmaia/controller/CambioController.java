9e1c7b9e5601c2dd3dfcf9d6576b0a9cf42389a6
Rafael Maia <106180433+rafaelmaiia@users.noreply.github.com>
2024-11-29 18:37:28 -0300

    configure Swagger OpenAPI for Cambio Service with custom API information and operation tags

--- src/main/java/br/com/rafaelmaia/controller/CambioController.java
+++ src/main/java/br/com/rafaelmaia/controller/CambioController.java
@@ -12,7 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.rafaelmaia.model.Cambio;
 import br.com.rafaelmaia.repository.CambioRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
+@Tag(name = "Cambio Service API")
 @RestController
 @RequestMapping("cambio-service")
 public class CambioController {
@@ -23,7 +26,7 @@ public class CambioController {
 	@Autowired
 	private CambioRepository repository;
 	
-	// http://localhost:8000/cambio-service/5/USD/BRL
+	@Operation(summary = "Get cambio from currency")
 	@GetMapping(value = "/{amount}/{from}/{to}")
 	public Cambio getCambio(
 			@PathVariable("amount") BigDecimal amount,
